7dc35980627de7acea5a7f1c69e7acb61e187db4
Baalon <bowdelderobert@laposte.net>
2016-04-03 22:29:58 +0200

    modified benchmark with STORE, LOAD, ADD, SUB, MULT, AND, OR

--- benchmark_eda.vhd
+++ benchmark_eda.vhd
@@ -30,29 +30,210 @@ begin
     runp_in <= '1';
     resetp_in <= '1';
 
- -- test MVI de 1 dans R3
+ -- test MVI de 3 dans R3
     Dinp_in <= "0000011000000000";
     clkp_in <= '1';
 
-    -- On met la valeur 1 dans Din
-    Dinp_in <= "0000000000000001";
+    -- On met la valeur 3 dans Din
+    Dinp_in <= "0000000000000011";
     clkp_in <= '0';
 
     wait for 1ns;
     assert(donep_out='1') report "Fail MVI/done/1" severity error;
-	assert(busp_out="0000000000000001") report "Fail MVI/busp/1" severity error;
+	assert(busp_out="0000000000000011") report "Fail MVI/busp/3" severity error;
  -- fin test MVI
 
+
+
+ -- test STORE de 2 dans RStore
+    Dinp_in <= "0010000000000000";
+    clkp_in <= '1';
+
+    -- On met la valeur 2 dans Din
+    Dinp_in <= "0000000000000010";
+    clkp_in <= '0';
+
+    wait for 1ns;
+    assert(donep_out='1') report "Fail STORE/done/1" severity error;
+	assert(busp_out="0000000000000010") report "Fail STORE/busp/2" severity error;
+ -- fin test STORE
+
+
+
  -- test MV de R3 dans R1
---    Dinp_in <= "0001001011000000";
---    clkp_in <= '1';
+    Dinp_in <= "0001001011000000";
+    clkp_in <= '1';
 
---    clkp_in <= '0';
+    clkp_in <= '0';
 
---    wait for 1ns;
---   assert(donep_out='1') report "Fail MV/done/1" severity error;
---	assert(busp_out="0000000000000001") report "Fail MVI/busp/1" severity error;
+    wait for 1ns;
+   assert(donep_out='1') report "Fail MV/done/1" severity error;
+	assert(busp_out="0000000000000011") report "Fail MVI/busp/3" severity error;
  -- fin test MV
+
+
+ -- test LOAD de RStore (2) sur le bus
+    Dinp_in <= "0011000000000000";
+    clkp_in <= '1';
+
+    clkp_in <= '0';
+
+    wait for 1ns;
+    assert(donep_out='1') report "Fail LOAD/done/1" severity error;
+	assert(busp_out="0000000000000010") report "Fail LOAD/busp/2" severity error;
+ -- fin test LOAD
+
+
+
+
+ -- test ADD de R1 et R3 dans R1
+    Dinp_in <= "0100001011000000";
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+	-- lecture de R1 (3) et ecriture dans A
+    wait for 1ns;
+    assert(donep_out='0') report "Fail ADD/done/0_etape1" severity error;
+	assert(busp_out="0000000000000011") report "Fail ADD/busp/3_etape1" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de R3 (3) et ecriture dans G
+    wait for 1ns;
+    assert(donep_out='0') report "Fail ADD/done/0_etape2" severity error;
+	assert(busp_out="0000000000000011") report "Fail ADD/busp/3_etape2" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de G (6) et écriture dans R1
+    wait for 1ns;
+    assert(donep_out='1') report "Fail ADD/done/1_etape3" severity error;
+	assert(busp_out="0000000000000110") report "Fail ADD/busp/6_etape3" severity error;
+
+
+ -- fin test ADD
+
+
+
+ -- test MULT de R1 et R3 dans R1
+    Dinp_in <= "0110001011000000";
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+	-- lecture de R1 (6) et ecriture dans A
+    wait for 1ns;
+    assert(donep_out='0') report "Fail MULT/done/0_etape1" severity error;
+	assert(busp_out="0000000000000110") report "Fail MULT/busp/6_etape1" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de R3 (3) et ecriture dans G
+    wait for 1ns;
+    assert(donep_out='0') report "Fail MULT/done/0_etape2" severity error;
+	assert(busp_out="0000000000000011") report "Fail MULT/busp/3_etape2" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de G (18) et écriture dans R1
+    wait for 1ns;
+    assert(donep_out='1') report "Fail MULT/done/1_etape3" severity error;
+	assert(busp_out="0000000000010010") report "Fail MULT/busp/18_etape3" severity error;
+
+ -- fin test MULT
+
+
+
+ -- test SUB de R1 et R3 dans R1
+    Dinp_in <= "0101001011000000";
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+	-- lecture de R1 (18) et ecriture dans A
+    wait for 1ns;
+    assert(donep_out='0') report "Fail SUB/done/0_etape1" severity error;
+	assert(busp_out="0000000000010010") report "Fail SUB/busp/18_etape1" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de R3 (3) et ecriture dans G
+    wait for 1ns;
+    assert(donep_out='0') report "Fail SUB/done/0_etape2" severity error;
+	assert(busp_out="0000000000000011") report "Fail SUB/busp/3_etape2" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de G (15) et écriture dans R1
+    wait for 1ns;
+    assert(donep_out='1') report "Fail SUB/done/1_etape3" severity error;
+	assert(busp_out="0000000000001111") report "Fail SUB/busp/15_etape3" severity error;
+
+ -- fin test SUB
+
+
+ -- test OR de R1 et R3 dans R1
+    Dinp_in <= "1001001011000000";
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+	-- lecture de R1 (15) et ecriture dans A
+    wait for 1ns;
+    assert(donep_out='0') report "Fail OR/done/0_etape1" severity error;
+	assert(busp_out="0000000000001111") report "Fail OR/busp/15_etape1" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de R3 (3) et ecriture dans G
+    wait for 1ns;
+    assert(donep_out='0') report "Fail OR/done/0_etape2" severity error;
+	assert(busp_out="0000000000000011") report "Fail OR/busp/3_etape2" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de G (15) et écriture dans R1
+    wait for 1ns;
+    assert(donep_out='1') report "Fail OR/done/1_etape3" severity error;
+	assert(busp_out="0000000000001111") report "Fail OR/busp/15_etape3" severity error;
+
+ -- fin test OR
+
+
+ -- test AND de R3 et R1 dans R3
+    Dinp_in <= "1000011001000000";
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+
+	-- lecture de R3 (3) et ecriture dans A
+    wait for 1ns;
+    assert(donep_out='0') report "Fail AND/done/0_etape1" severity error;
+	assert(busp_out="0000000000000011") report "Fail AND/busp/3_etape1" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de R1 (15) et ecriture dans G
+    wait for 1ns;
+    assert(donep_out='0') report "Fail AND/done/0_etape2" severity error;
+	assert(busp_out="0000000000001111") report "Fail AND/busp/15_etape2" severity error;
+
+    clkp_in <= '1';
+    clkp_in <= '0';
+
+    -- lecture de G (15) et écriture dans R1
+    wait for 1ns;
+    assert(donep_out='1') report "Fail AND/done/1_etape3" severity error;
+	assert(busp_out="0000000000000011") report "Fail AND/busp/3_etape3" severity error;
+
+ -- fin test AND
+
 
     assert false report "Test done." severity note;
     wait;
